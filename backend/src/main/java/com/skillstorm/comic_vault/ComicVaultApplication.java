8e99ea2bac169e59275875bb5b6fdf2500195b9c
ericpark25 <110264234+ericpark25@users.noreply.github.com>
2025-12-09 17:44:18 -0500

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/skillstorm/comic_vault/ComicVaultApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.comic_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicVaultApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/skillstorm/comic_vault/ComicVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.comic_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
