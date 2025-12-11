1845401b2d09732856e0c79462cb34ec81ba7f9d
ericpark25 <110264234+ericpark25@users.noreply.github.com>
2025-12-11 02:46:25 -0500

    Initialize front-end + setup dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <h1 className='text-3xl font-bold underline'>Comic Vault</h1>
+            <p>inventory management system</p>
+        </>
+    );
+}
+export default App;
